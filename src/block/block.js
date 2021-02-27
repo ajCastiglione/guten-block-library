c2573a6f5fe621bbad7aacd371ff28fc69513666
Antonio Castiglione <antonio@minervawebdevelopment.com>
2021-02-26 20:50:08 -0500

    built a simple card block with basics

--- src/block/block.js
+++ src/block/block.js
@@ -12,6 +12,8 @@ import "./style.scss";
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 
+const { RichText, MediaUpload, PlainText } = wp.editor;
+const { Button } = wp.components;
 /**
  * Register: a Gutenberg Block.
  *
@@ -25,77 +27,134 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType("cgb/block-custom-blocks", {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __("custom-blocks - CGB Block"), // Block title.
-	icon: "shield", // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: "common", // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	keywords: [
-		__("custom-blocks — CGB Block"),
-		__("CGB Example"),
-		__("create-guten-block"),
-	],
+registerBlockType("mwd/card", {
+    // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+    title: __("Card"), // Block title.
+    icon: "shield", // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+    category: "common", // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+    keywords: [__("card"), __("card block"), __("mwd")],
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Component.
-	 */
-	edit: (props) => {
-		// Creates a <p class='wp-block-cgb-block-custom-blocks'></p>.
-		return (
-			<div className={props.className}>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>custom-blocks</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{" "}
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>
-					.
-				</p>
-			</div>
-		);
-	},
+    attributes: {
+        title: {
+            source: "text",
+            selector: ".card__title",
+        },
+        body: {
+            type: "array",
+            source: "children",
+            selector: ".card__body",
+        },
+        imageAlt: {
+            attribute: "alt",
+            selector: ".card__image",
+        },
+        imageUrl: {
+            attribute: "src",
+            selector: ".card__image",
+        },
+    },
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Frontend HTML.
-	 */
-	save: (props) => {
-		return (
-			<div className={props.className}>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>custom-blocks</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{" "}
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>
-					.
-				</p>
-			</div>
-		);
-	},
+    /**
+     * The edit function describes the structure of your block in the context of the editor.
+     * This represents what the editor will render when the block is used.
+     *
+     * The "edit" property must be a valid function.
+     *
+     * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+     *
+     * @param {Object} props Props.
+     * @returns {Mixed} JSX Component.
+     */
+    edit: ({ attributes, className, setAttributes }) => {
+        // Creates a <p class='wp-block-cgb-block-custom-blocks'></p>.
+        const getImageButton = (openEvent) => {
+            if (attributes.imageUrl) {
+                return (
+                    <img
+                        src={attributes.imageUrl}
+                        onClick={openEvent}
+                        className="image"
+                    />
+                );
+            } else {
+                return (
+                    <div className="button-container">
+                        <Button
+                            onClick={openEvent}
+                            className="button button-large">
+                            Pick an image
+                        </Button>
+                    </div>
+                );
+            }
+        };
+
+        return (
+            <div className="container">
+                <MediaUpload
+                    onSelect={(media) => {
+                        setAttributes({
+                            imageAlt: media.alt,
+                            imageUrl: media.url,
+                        });
+                    }}
+                    type="image"
+                    value={attributes.imageID}
+                    render={({ open }) => getImageButton(open)}
+                />
+                <PlainText
+                    onChange={(content) => setAttributes({ title: content })}
+                    value={attributes.title}
+                    placeholder="Your card title goes here..."
+                    className="heading"
+                />
+                <RichText
+                    onChange={(content) => setAttributes({ body: content })}
+                    value={attributes.body}
+                    multiline="p"
+                    placeholder="Your card body here..."
+                />
+            </div>
+        );
+    },
+
+    /**
+     * The save function defines the way in which the different attributes should be combined
+     * into the final markup, which is then serialized by Gutenberg into post_content.
+     *
+     * The "save" property must be specified and must be a valid function.
+     *
+     * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+     *
+     * @param {Object} props Props.
+     * @returns {Mixed} JSX Frontend HTML.
+     */
+    save: ({ attributes }) => {
+        const cardImage = (src, alt) => {
+            if (!src) return null;
+
+            if (alt) {
+                return <img className="card__image" src={src} alt={alt} />;
+            }
+
+            return (
+                <img
+                    className="card__image"
+                    src={src}
+                    alt=""
+                    aria-hidden="true"
+                />
+            );
+        };
+
+        return (
+            <div className="card">
+                {cardImage(attributes.imageUrl, attributes.imageAlt)}
+                <div className="card__content">
+                    <h3 className="card__title">{attributes.title}</h3>
+                    <div className="card__body">{attributes.body}</div>
+                </div>
+            </div>
+        );
+    },
 });
